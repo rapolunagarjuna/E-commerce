c27fe39d76a2a33bdc423a612158ff4ca982fa4f
Nikhita Rapolu <78137667+niki2212@users.noreply.github.com>
2023-07-18 23:50:23 +0530

    Add files via upload

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import fbIcon from '../assets/images/fbimg.png';
+import twitterIcon from '../assets/images/twitterimg.png';
+import LinkedinIcon from '../assets/images/linkedinimg.png';
+import instagramIcon from '../assets/images/instaimg.png';
 
-export default function Footer(){
-    return(
-        <div className='bg-slate-900 w-full h-96'>
-
+export default function Footer() {
+  return (
+    <div className='bg-slate-900 w-full h-64 px-12'>
+      <div className='flex justify-center h-full'>
+        <div className='w-1/4 p-16'>
+          <h3 className= 'text-white font-bold'>Products</h3>
+          <ul className='mt-2 text-gray-300 text-sm'>
+            <li>Geosynthetics</li>
+            <li>Erosion Control</li>
+            <li>Agro Textiles</li>
+            <li>Industrial Textiles</li>
+            <li>Packaging Textiles</li>
+            <li>Accessories</li>
+          </ul>
         </div>
-    )
+        <div className='w-1/8 p-16'>
+          <h3 className='text-white font-bold'>About Us</h3>
+          <ul className='mt-2 text-gray-300 text-sm'>
+            <li>Company</li>
+            <li>Location</li>
+            <li>Team</li>
+
+          </ul>
+        </div>
+        <div className='w-1/4 p-16'>
+          <h3 className='text-white font-bold'>Socials</h3>
+          <ul className='mt-2 flex items-center text-gray-300 text-sm'>
+            <li><img className='h-6 w-6 mr-2' src={fbIcon} alt='Facebook' /></li>
+            <li><img className='h-6 w-6 mr-2' src={twitterIcon} alt='Twitter' /></li>
+            <li><img className='h-6 w-6 mr-2' src={instagramIcon} alt='Instagram' /></li>
+            <li><img className='h-6 w-6 mr-2' src={LinkedinIcon} alt='LinkedIn' /></li>
+          </ul>
+        </div>
+        <div className='w-1/3 p-16'>
+          <h4 className='text-white font-bold'>Contact Us</h4>
+          <p className='mt-2 text-gray-300 text-sm'>770-544-7392</p>
+          <p className='mt-1 text-gray-300 text-sm'>sales@parglobal.us</p>
+        </div>
+      </div>
+    </div>
+  );
 }
