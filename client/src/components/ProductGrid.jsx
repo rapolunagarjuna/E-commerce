ab2ba89a2ddeb0f92d563a7cb0787f04c4c8537d
Nikhita Rapolu <78137667+niki2212@users.noreply.github.com>
2023-07-20 15:05:55 +0530

    Update ProductGrid.jsx

--- client/src/components/ProductGrid.jsx
+++ client/src/components/ProductGrid.jsx
@@ -1,39 +1,44 @@
 import { useNavigate } from 'react-router-dom';
 import BlueBtn from './BlueBtn'
-import images from '../assets/images/products-bg.jpeg';
+import image1 from '../assets/images/geosynthetics.jpg';
+import image2 from '../assets/images/erosioncontrol.jpg';
+import image3 from '../assets/images/industrialtextiles.jpg';
+import image4 from '../assets/images/agrotextiles.jpg';
+import image5 from '../assets/images/packagingtextiles.jpg';
+import image6 from '../assets/images/accessories.jpg';
 
 const products = [
     {
         name: 'Geosynthetics', 
         description: "Our geosynthetics are designed to enhance soil performance, providing reinforcement, separation, filtration, and containment solutions for civil engineering projects.",
         link: '/products/geosynthetics',
-        imgSrc: images
+        imgSrc: image1
     },
     {
         name: 'Erosion Control',
         description: "Our erosion control products are designed to protect and stabilize soil surfaces against erosion and environmental factors.",
         link: '/products/erosion-control',
-        imgSrc: images
+        imgSrc: image2
     },
     {   name: 'Industrial Textiles', 
         description: "For industrial applications, our industrial textiles are engineered to meet the demanding requirements of various industries, offering strength, durability, and reliability.",
         link: '/products/industrial-textiles',
-        imgSrc: images
+        imgSrc: image3
     },
     {   name: 'Agro Textiles', 
         description: " Our agro textiles cater to the agricultural sector, providing innovative solutions for crop protection, soil stabilization, and irrigation.",
         link: '/products/agro-textiles',
-        imgSrc: images
+        imgSrc: image4
     },
     {   name: 'Packaging Textiles', 
         description: " We offer packaging textiles designed to ensure the safe and efficient transport of goods, as well as a range of accessories to complement our product offerings.",
         link: '/products/packaging-textiles',
-        imgSrc: images 
+        imgSrc: image5
     },
     {   name: 'Accessories', 
         description: "Our accessories are designed to enhance the quality of our products and services.",
         link: '/products/accessories',
-        imgSrc: images
+        imgSrc: image6
     }
 ]
 
@@ -47,7 +52,7 @@ function Item({name, description, link ,imgSrc}) {
         <div className="flex flex-row justify-center p-5 h-full">
 
             <div className="w-96 h-full ">
-                <img src={imgSrc} className="object-cover w-full h-full" alt=" " />
+                <img src={imgSrc} className="w-full h-full object-cover" alt=" " />
             </div>
 
 
