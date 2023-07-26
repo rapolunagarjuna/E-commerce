eee3b950281f3e56042789320f1e29f95ec71191
Nikhita Rapolu <78137667+niki2212@users.noreply.github.com>
2023-07-27 03:05:14 +0530

    Add files via upload

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,18 @@ import Navbar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import Footer from '../components/Footer';
 import BlueBtn from "../components/BlueBtn";
+import WhyChooseParSection from "../components/WhyChooseParSection";
+import ProductDisplay from "../components/ProductDisplay";
+import NewsEventSection from "../components/NewsEventSection";
 
 export default function Home() {
 
   return (
     <div>
       <HeroSection/>
+      <ProductDisplay/>
+      <WhyChooseParSection />
+      <NewsEventSection />
       <Footer />
     </div>
     
